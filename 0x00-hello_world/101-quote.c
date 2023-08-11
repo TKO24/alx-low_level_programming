bf7341983b299dc2a626439a158264e456863b8c
Tshepo Olefa <tshepo.jr19@gmail.com>
2023-08-10 18:34:05 -0700

    101

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
 #include <unistd.h>
 /**
- * main - Function
- * Return: 1 (Success)
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
  */
-int main(void) 
+int main(void)
 {
-fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+fputs("and that piece of art is useful\" - Dora Korpar, 015-10-19\n", stdout);
 return (1);
 }
