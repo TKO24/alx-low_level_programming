c72376b69b55573c7a33f2ad86f96d1d5d87d33d
Tshepo Olefa <tshepo.jr19@gmail.com>
2023-08-14 13:30:05 -0700

    6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - A function that print if a number is positive, negative or zero
+ * Return: Always (Success)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = '0'; i <= '9'; i++)
+	putchar(i);
+	putchar('\n');
+
+	return (0);
+}
