cb4920bbebe1030486bddb0b41f504e17be0d61f
Tshepo Olefa <tshepo.jr19@gmail.com>
2023-08-14 13:42:47 -0700

    7

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - A function that print if a number is positive, negative or zero
+ * Return: Always (Success)
+ */
+int main(void)
+{
+char ch;
+
+for (ch = 'z'; ch >= 'a'; ch--)
+{
+putchar(ch);
+}
+putchar('\n');
+return (0);
+}
