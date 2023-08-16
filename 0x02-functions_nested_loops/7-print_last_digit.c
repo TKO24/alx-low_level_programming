82d17f1a13d47886913835d987874c34c705aa36
Tshepo Olefa <tshepo.jr19@gmail.com>
2023-08-16 10:53:34 -0700

    8

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_last_digit - print last digit
+ * @n: input
+ * Return: last digit
+ */
+
+int print_last_digit(int n)
+{
+	int ld;
+
+	ld = n % 10;
+	if (ld < 0)
+	{
+		ld = ld * -1;
+	}
+	_putchar(ld + '0');
+	return (ld);
+}
