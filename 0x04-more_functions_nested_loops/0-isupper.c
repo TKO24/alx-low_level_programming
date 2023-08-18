a18fc45dd1683555ff20dd7ace83ef4af483fa27
Tshepo Olefa <tshepo.jr19@gmail.com>
2023-08-18 13:24:58 -0700

    0

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _isupper - checks uppercase
+ * @c: input variable
+ * Return: 0
+ */
+
+int _isupper(int c)
+{
+	if (c >= 'A' && c <= 'Z')
+	{
+		return (i);
+	}
+	else
+	{
+		return (0);
+	}
+}
