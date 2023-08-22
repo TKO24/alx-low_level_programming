3aef5367561baf8a57b1503e35a34ff89f440c93
Tshepo Olefa <tshepo.jr19@gmail.com>
2023-08-22 15:28:29 -0700

    3

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,15 +1,16 @@
 #include "main.h"
 
 /**
- * _puts - a function that prints a string, followed by a new line, to stdout.
+ * _puts -  a function that prints a string, followed by a new line
  * @str: input
+ * Return: 0
  */
 
-void print_rev(char *s)
+void _puts(char *str)
 {
-	while (*str != '\0')
+	for (; *str != '\0'; str++)
 	{
-		_putchar(*str++);
+		_putchar(*str);
 	}
 	_putchar('\n');
 }
